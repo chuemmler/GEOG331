86b9088f294734d632001a9fa72abe33ee1004db
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-01-24 12:11:47 -0500

    description of changes\

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,1 +1,12 @@
-print("hello R world")
+#activity 1
+#3 calculations
+2+4
+8/2
+5^5
+
+#Creates vector with 4 numbers and variable name.
+myVec <- c(1,2,3,4,5)
+
+#different vector operations
+myVec^2
+myVec + 2 
