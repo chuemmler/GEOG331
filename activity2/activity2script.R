eab982dea70ce47540638caa530cfcb1dba86c88
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-01-29 12:12:03 -0500

    Description of changes

--- activity2/activity2script.R
+++ activity2/activity2script.R
@@ -35,3 +35,71 @@ datW <- read.csv("y:\\Students\\hkropp\\a02\\2011124.csv")
 #get more information about the dataframe
 str(datW)
 
+#Question1
+#finds the number of columns and rows within the data 
+ncol(datW)
+nrow(datW)
+
+#specify a column with a proper date format
+#note the format here dataframe$column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#google date formatting in r to find more options and learn more
+
+#create a date column by reformatting the date to only include years
+#and indicating that it should be treated as numeric data
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+
+#Question 2
+#character vector
+charvec <- c("Cloud9","Fnatic","TSM","Liquid","Gravity")
+
+#numeric vector
+numvec <- c(3.5,999,563.4,0.44,12)
+
+#int vector
+intvec <- as.integer(c(1,2,3,4,5))
+
+#factor vector
+facvec <- as.factor(charvec)
+
+
+#find out all unique site names
+levels(datW$NAME)
+
+#look at the mean maximum temperature for Aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
+
+#calculate the average daily temperature
+#This temperature will be halfway between the minimum and maximum temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
+
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
+
+#change the automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("NAME","MAAT")
+averageTemp
+
+#convert level to number for factor data type
+#you will have to reference the level output or look at the row of data to see the character designation.
+datW$siteN <- as.numeric(datW$NAME)
+
+
+#make a histogram for the first site in our levels
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
