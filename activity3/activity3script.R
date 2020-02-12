30f02a059f44e08d9620ec05ceeefd283ac7e072
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-02-12 12:10:53 -0500

    Description of Changes

---
+++ activity3/activity3script.R
@@ -0,0 +1,33 @@
+#Activity 3 
+#Charlie Huemmler
+#2/12/2020
+
+#install packages
+#install.packages("lubridate")
+library(lubridate)
+
+
+
+#create a function. The names of the arguements for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
+assert <- function(statement,err.message){
+  #if evaluates if a statement is true or false for a single item
+  if(statement == FALSE){
+    print(err.message)
+  }
+  
+}
+
+#check how the statement works
+#evaluate a false statement
+assert(1 == 2, "error: unequal values")
+
+#evaluate a true statement
+assert(2 == 2, "error: unequal values")
+
+
+
+#set up assert to check if two vectors are the same length
+a <- c(1,2,3,4)
+b <- c(8,4,5)
+assert(length(a) == length(b), "error: unequal length")
+
