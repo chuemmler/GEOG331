1d1f095c8eb469bb25b5a9d3b77aaea5af3aa874
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-02-17 12:10:06 -0500

    Description of Changes

--- activity3/activity3script.R
+++ activity3/activity3script.R
@@ -5,7 +5,8 @@
 #install packages
 #install.packages("lubridate")
 library(lubridate)
-
+#install.packages("tidyverse")
+library(tidyverse)
 
 
 #create a function. The names of the arguements for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
@@ -14,7 +15,6 @@ assert <- function(statement,err.message){
   if(statement == FALSE){
     print(err.message)
   }
-  
 }
 
 #check how the statement works
@@ -31,3 +31,39 @@ a <- c(1,2,3,4)
 b <- c(8,4,5)
 assert(length(a) == length(b), "error: unequal length")
 
+
+
+
+#read in the data file
+#skip the first 3 rows since there is additional column info
+#specify the the NA is designated differently
+datW <- read.csv("y:\\Students\\hkropp\\a03\\bewkes_weather.csv",
+                 na.strings=c("#N/A"), skip=3, header=FALSE)
+#preview data
+print(datW[1,])
+
+
+sensorInfo <-   read.csv("y:\\Students\\hkropp\\a03\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
