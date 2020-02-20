1ede6718968f54917d3d7e9acb58cbb1c16bce13
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-02-19 21:59:58 -0500

    Description of Changes

--- activity3/activity3script.R
+++ activity3/activity3script.R
@@ -67,3 +67,95 @@ datW$hour <- hour(dates) + (minute(dates)/60)
 datW$DD <- datW$doy + (datW$hour/24)
 #quick preview of new date calcualtions
 datW[1,]
+
+
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+length(which(is.na(datW$air.tempQ1)))
+
+
+#wind speed
+length(which(is.na(datW$wind.speed)))
+
+#precipitation
+length(which(is.na(datW$precipitation)))
+
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+
+#I'm going to make a new column to work with that indicates that I am conducting QAQC
+#because overwriting values should be done cautiously and can lead to confusing issues.
+#It can be particularily confusing when you are just learning R.
+#Here I'm using the ifelse function
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,] 
+
+
+
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
+
+assert(length(lightscale) == nrow(datW),err.message = "Bad")
+
+
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+datW$wind.speed2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                           ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+for(i in 1:nrow(datW)){
+  assert(is.na(datW$wind.speed2[i]), "Not the Same")
+}
+
+plot(datW$DD, datW$wind.speed2, pch=19, type="b", xlab = "Day of Year",
+     ylab="Wind Speed")
+
+#investigating the errors in soil moisture and temp
+
--- activity4/activity4script.R
+++ activity4/activity4script.R
@@ -1,3 +1,5 @@
+#Charlie Huemmler
+
 #use built in iris dataset
 #take a look at it 
 head(iris)
