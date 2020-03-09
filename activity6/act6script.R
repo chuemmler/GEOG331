b2b1d9f948182d4430a9387585b0ceebbe8a2531
Charlie Huemmler '20 <chuemmler@colgate.edu>
2020-03-09 12:12:01 -0400

    Description of Changes

--- activity6/act6script.R
+++ activity6/act6script.R
@@ -1,7 +1,7 @@
 #Activity 6 "the big one"
 #Charlie Huemmler
 
-install.packages(c("raster","sp","rgdal","rgeos","plyr"))
+#install.packages(c("raster","sp","rgdal","rgeos","plyr"))
 
 library(raster)
 library(sp)
@@ -14,3 +14,60 @@ library(plyr)
 g1966 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_1966.shp")
 str(g1966)
 plot(g1966, col = "black", axes = T)
+
+
+g1998 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_1998.shp")
+g2005 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_2005.shp")
+g2015 <- readOGR("Y:\\Students\\hkropp\\a06\\GNPglaciers\\GNPglaciers_2015.shp")
+
+#some code may be missing here
+
+redL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_red.tif")
+greenL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_green.tif")
+blueL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_blue.tif")
+
+
+rgbL <- brick(redL, greenL, blueL)
+
+#plot with color
+#show axes for reference
+#add contrast to the imagery to see it better
+par(mai=c(1,1,1,1))
+plotRGB(rgbL, stretch="lin", axes=TRUE)
+#add polygons to plot
+plot(g1966, col="tan3", border=NA, add=TRUE)
+plot(g1998, col="royalblue3", add=TRUE, border=NA)
+plot(g2005, col="darkgoldenrod4", add=TRUE, border=NA)
+plot(g2015, col="tomato3", add=TRUE, border=NA)
+
+#zoom in on an area using the ext function
+plotRGB(rgbL, ext=c(289995,310000,5371253,5400000), stretch="lin")
+plot(g1966, col="palegreen2", border=NA, add=TRUE)
+plot(g1998, col="royalblue3", add=TRUE, border=NA)
+plot(g2005, col="darkgoldenrod4", add=TRUE, border=NA)
+plot(g2015, col="tomato3", add=TRUE, border=NA)
+
+
+#set up years to read in
+ndviYear <- seq(2003,2016)
+
+#read all files into a list
+NDVIraster <- list() 
+for(i in 1:length(ndviYear)){
+  NDVIraster[[i]] <- raster(paste0("Y:\\Students\\hkropp\\a06\\NDVI\\NDVI_",ndviYear[i],".tif"))
+  
+}
+
+#higher NDVI represtents more vegitation on the ground
+plot(NDVIraster[[1]])
+
+str(NDVIraster[[1]])
+
+NDVIraster[[1]]@crs
+
+
+#question 3
+par(mfrow = c(1,2))
+#par(mai=c(1,1,1,1))
+plot(NDVIraster[[1]], axes=TRUE)
+plot(g1966, axes=TRUE)
